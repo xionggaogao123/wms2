cc16acf47d36065ba062c106ad962c3ce8f8065d
Jztdebu <1156022832@qq.com>
2022-01-02 19:24:59 +0800

    2021/01/02  物料分类和物料基础信息修改  附件上传、查询、删除。

---
+++ src/main/java/com/huanhong/common/handler/MyMetaObjectHandler.java
@@ -0,0 +1,23 @@
+package com.huanhong.common.handler;
+
+import cn.hutool.core.date.DateUtil;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        //this.strictInsertFill(metaObject, "createTime", Date.class, new Date()); // 起始版本 3.3.0(推荐使用)s
+        this.setFieldValByName("createTime", DateUtil.toLocalDateTime(new Date()),metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+
+    }
+}
